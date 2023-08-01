f51ce2b257ff161d8a0ef7d2d1d97b59ad5a403d
Victoria Shaitorova <shajtorova@mail.ru>
2023-08-01 07:32:00 +0300

    Initial commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.NoSuchElementException;
+
+public abstract class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+    public static final String BASE_URL = "https://app.qase.io/signup";
+
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver,10);
+    }
+
+    public abstract boolean isPageOpen();
+
+    public boolean isExist(By locator){
+        try {
+            return driver.findElement(locator).isDisplayed();
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+}
---
+++ src/test/java/pages/SignupPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignupPage extends BasePage{
+    public static final By SIGN_UP_BUTTON = By.xpath("//button[@type='submit']");
+    public static final By E_MAIL = By.xpath("//input[@placeholder='E-Mail']");
+    public static final By PASSWORD = By.xpath("//input[@placeholder='Password']");
+    public static final By PASSWORD_CONFIRMATION = By.xpath("//input[@placeholder='Password confirmation']");
+    String informationalMessage = "//small[@class='VV3w3Z']";
+    public SignupPage(WebDriver driver) {
+        super(driver);
+    }
+    public SignupPage open(){
+        driver.get(BASE_URL);
+        return this;
+    }
+    public SignupPage fillInRegistrationFieldsWithValidData(String email, String password, String passwordConfirmation){
+        driver.findElement(E_MAIL).sendKeys(email);
+        driver.findElement(PASSWORD).sendKeys(password);
+        driver.findElement(PASSWORD_CONFIRMATION).sendKeys(passwordConfirmation);
+        return this;
+    }
+
+    public SignupPage clickSignUpButton(){
+        driver.findElement(SIGN_UP_BUTTON).click();
+        return this;
+    }
+
+
+
+    @Override
+    public boolean isPageOpen() {
+        return isExist(SIGN_UP_BUTTON);
+    }
+}
